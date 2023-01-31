8e0b1cc64ac6cdafa2bc213a94eda9cfa2198274
merTaner <merttaner44@gmail.com>
2023-01-31 15:20:26 +0300

    update file

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def home():
 
     return render_template('home.html', data = data)
 
+
 @app.route('/add_product', methods=["POST"])
 def add_product():
     uom_id = request.form['uom_id']
@@ -24,6 +25,7 @@ def add_product():
     connection.commit()
 
     return redirect(url_for('home'))
+
 
 @app.route('/update', methods=["POST"])
 def update():
@@ -53,6 +55,75 @@ def delete(id):
     return redirect(url_for('home'))
 
 
+
+@app.route('/order', methods=["GET", "POST"])
+def order():
+    
+    if request.method == "POST":
+        cursor = connection.cursor()
+
+        customer_name_ = request.form['Customer Name']
+        total_ = request.form["Total"]
+        date_ = request.form["Date"]
+
+        cursor.execute("""INSERT INTO ORDERS (customer_name, total, date)
+                          VALUES (%s, %s, %s);""", (customer_name_, total_, date_))
+
+        connection.commit()
+        
+        return redirect(url_for('order'))
+
+    else:
+        cursor = connection.cursor()
+        cursor.execute(""" SELECT * FROM ORDERS """)
+        orders = cursor.fetchall()
+
+        return render_template('order.html', data=orders)
+
+
+@app.route('/deleteorder/<id>', methods=["GET"])
+def delete_order(id):
+    cursor = connection.cursor()
+    cursor.execute("""
+                    DELETE FROM ORDERS 
+                    WHERE ORDER_ID= %s;
+                """, (id,))
+    connection.commit()
+
+    return redirect(url_for('order'))
+
+
+@app.route('/orderdetail', methods=["GET"])
+def order_detail():
+   
+    cursor = connection.cursor()
+    cursor.execute(""" 
+                        SELECT 
+                                PRODUCTS.PRODUCTS_ID, PRODUCTS.NAME, PRODUCTS.UNIT_PRICE, AMOUNT, TOTAL_PRICE
+                            FROM
+                                ORDER_DETAILS
+                            RIGHT JOIN PRODUCTS ON PRODUCTS.PRODUCTS_ID = ORDER_DETAILS.PRODUCT_ID
+                            LEFT JOIN ORDERS ON ORDERS.ORDER_ID = ORDER_DETAILS.ORDER_ID 
+                  """)
+    order_detail_ = cursor.fetchall() 
+        
+    return render_template('order.html', orderdetail=order_detail_)
+
+  
+    
+
+
+
+# run controller
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+""""
+# Error page
 @app.errorhandler(404)
 def page_not_found(error):  
     return render_template('page_not_found.html'), 404
@@ -61,6 +132,4 @@ def page_not_found(error):
 def page_not_found(error):  
     return render_template('not-found-500.html'), 500
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+"""
